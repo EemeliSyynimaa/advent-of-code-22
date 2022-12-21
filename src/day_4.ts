29b9358aa0ebd1d80fdf11a97f1660ee81affa59
Eemeli Syynimaa <eemeli.syynimaa@kapsi.fi>
2022-12-21 09:31:15 +0200

    Add tslint

--- src/day_4.ts
+++ src/day_4.ts
@@ -1,40 +1,42 @@
 import * as fs from 'node:fs';
 
-function main()
+function main() : void
 {
-    let data = fs.readFileSync("data/4.txt", "utf8");
-    let values = data.split('\n');
+    const data = fs.readFileSync("data/4.txt", "utf8");
+    const values = data.split('\n');
 
     if (values.length)
     {
-        let totalA : number = 0;
-        let totalB : number = 0;
+        let totalA = 0;
+        let totalB = 0;
 
-        for (let value in values)
+        for (const value in values)
         {
-            let sections : string[] = values[value].split(',');
+            const sections : string[] = values[value].split(',');
 
             if (sections.length > 1)
             {
                 console.log(sections);
 
-                let rangeA = sections[0].split('-');
-                let rangeB = sections[1].split('-');
+                const rangeA = sections[0].split('-');
+                const rangeB = sections[1].split('-');
 
-                let minA : number = Number(rangeA[0]);
-                let maxA : number = Number(rangeA[1]);
-                let minB : number = Number(rangeB[0]);
-                let maxB : number = Number(rangeB[1]);
+                const minA = Number(rangeA[0]);
+                const maxA = Number(rangeA[1]);
+                const minB = Number(rangeB[0]);
+                const maxB = Number(rangeB[1]);
 
-                let includes : boolean = false;
+                let includes = false;
 
-                if ((minA >= minB && maxA <= maxB) || (minB >= minA && maxB <= maxA))
+                if ((minA >= minB && maxA <= maxB) ||
+                    (minB >= minA && maxB <= maxA))
                 {
                     includes = true;
                     totalA++;
                 }
 
-                if ((minA >= minB && minA <= maxB) || (minB >= minA && minB <= maxA))
+                if ((minA >= minB && minA <= maxB) ||
+                    (minB >= minA && minB <= maxA))
                 {
                     includes = true;
                     totalB++;
